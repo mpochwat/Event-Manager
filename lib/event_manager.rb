97ffec30101c47f457fed1d0fbb4a2f94341ae4b
Martin <martinochwat1@gmail.com>
2015-01-22 23:55:12 -0500

    Comments added

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,7 +9,7 @@ def clean_zipcode(zipcode)
 end
 
 def clean_phonenumber(homephone)
-  clean_num = homephone.gsub(/-|\.|\(|\)|\s+/, '')
+  clean_num = homephone.gsub(/-|\.|\(|\)|\s+/, '') # Remove off spacing
   if clean_num.length == 10
   	clean_num
   elsif clean_num.length == 11 && clean_num[0] == "1"
@@ -17,7 +17,7 @@ def clean_phonenumber(homephone)
   else
   	clean_num = ""
   end
-  # Converts phone num into standard format
+  # Converts phone num into xxx-xxx-xxxx format
   clean_num.sub(/(\d{3})(\d{3})(\d{4})/, "\\1-\\2-\\3")
 end
 
@@ -35,22 +35,22 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
-def hours_convert(hour)
-	if hour > 12
-		hour = "#{hour % 12}pm"
-	else
-		hour = "#{hour}am"
-	end
+def hour_convert(hour)
+  if hour > 12
+  	hour = "#{hour % 12}pm"
+  else
+	hour = "#{hour}am"
+  end
 end
 
 def registration_analytics(hash)
-  peak_array = ((hash.sort_by { |k, v| v}).reverse)
+  peak_array = ((hash.sort_by { |k, v| v }).reverse)
 
   puts ""
   puts "Peak registration hours:"
-  puts "#{peak_array[0][1]} people signed up at #{hours_convert(peak_array[0][0])}"
-  puts "#{peak_array[1][1]} people signed up at #{hours_convert(peak_array[1][0])}"
-  puts "#{peak_array[2][1]} people signed up at #{hours_convert(peak_array[2][0])}"
+  puts "#{peak_array[0][1]} people signed up at #{hour_convert(peak_array[0][0])}."
+  puts "#{peak_array[1][1]} people signed up at #{hour_convert(peak_array[1][0])}."
+  puts "#{peak_array[2][1]} people signed up at #{hour_convert(peak_array[2][0])}."
 end
 
 
@@ -61,7 +61,7 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
-registration_hours = Hash.new(0) # Used for registration hours analytics
+registration_hours = Hash.new(0) # Hash used for registration hours analytics
 
 contents.each do |row|
   id = row[0]
@@ -75,9 +75,9 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  # save_thank_you_letter(id, form_letter)
+  save_thank_you_letter(id, form_letter)
 
-  # Registration hours analytics
+  # Upades registration hours hash
   date = row[:regdate]
   format = "%m/%d/%y %H:%M"
   date_time = DateTime.strptime(date, format)
